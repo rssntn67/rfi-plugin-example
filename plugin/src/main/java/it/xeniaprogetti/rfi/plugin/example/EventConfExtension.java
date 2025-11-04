92eebff122ec1bc74d5b3e4d3e573c5d29c0c3b6
Tony Burgo <aburgo@xeniaprogetti.it>
2025-11-04 10:47:53 +0100

    first commit

---
+++ plugin/src/main/java/it/xeniaprogetti/rfi/plugin/example/EventConfExtension.java
@@ -0,0 +1,19 @@
+package it.xeniaprogetti.rfi.plugin.example;
+
+import java.util.List;
+
+import org.opennms.integration.api.v1.config.events.EventDefinition;
+import org.opennms.integration.api.xml.ClasspathEventDefinitionLoader;
+
+public class EventConfExtension implements org.opennms.integration.api.v1.config.events.EventConfExtension {
+
+    private final ClasspathEventDefinitionLoader classpathEventDefinitionLoader = new ClasspathEventDefinitionLoader(
+            EventConfExtension.class,
+            "plugin.ext.events.xml"
+    );
+
+    @Override
+    public List<EventDefinition> getEventDefinitions() {
+        return classpathEventDefinitionLoader.getEventDefinitions();
+    }
+}
