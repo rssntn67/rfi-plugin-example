14bb5ba9cee9576ad449aba503c435394932d11e
aburgo <aburgo@xeniaprogetti.it>
2025-11-11 09:57:15 +0100

    add EventSubscriptionService

--- plugin/src/main/java/it/xeniaprogetti/rfi/plugin/example/events/AComEventIngestor.java
+++ plugin/src/main/java/it/xeniaprogetti/rfi/plugin/example/events/AComEventIngestor.java
@@ -4,69 +4,88 @@ import org.opennms.integration.api.v1.dao.AlarmDao;
 import org.opennms.integration.api.v1.dao.NodeDao;
 import org.opennms.integration.api.v1.events.EventForwarder;
 import org.opennms.integration.api.v1.events.EventListener;
+import org.opennms.integration.api.v1.events.EventSubscriptionService;
 import org.opennms.integration.api.v1.model.InMemoryEvent;
 import org.opennms.integration.api.v1.model.Node;
 import org.opennms.integration.api.v1.model.immutables.ImmutableInMemoryEvent;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 
-public class AComEventIngestore implements EventListener, Runnable {
+public class AComEventIngestor implements EventListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AComEventIngestor.class);
 
 	private static String UEI_ACOM_MATCH = "uei.opennms.org/traps/INC-MIB-AL";
 	private static String NODE_LABEL_ACOM_PARAMETER_MATCH = ".1.3.6.1.4.1.231.7.99.4.2.1.1.11";
-	
-	private int threads;
+
 	private final EventForwarder eventForwarder;
 	private final NodeDao nodeDao;
 	private final AlarmDao alarmDao;
-	
+    private final EventSubscriptionService eventSubscriptionService;
+
 	private boolean syncAct = false;
 	private Date startSyncDate;
-	
-	public AComEventIngestore(int threads, EventForwarder eventForwarder, NodeDao nodeDao, AlarmDao alarmDao) {
-	
-		this.threads = threads;
-		this.eventForwarder = eventForwarder;
-		this.nodeDao = nodeDao;
-		this.alarmDao = alarmDao;
-		
-		//ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();		
-		//ScheduledFuture<ScheduledExecutorService>
-	}
-	
+
+    public AComEventIngestor(EventForwarder eventForwarder,
+                             NodeDao nodeDao,
+                             AlarmDao alarmDao,
+                             EventSubscriptionService eventSubscriptionService) {
+        // costruttore vuoto necessario
+        this.eventForwarder = eventForwarder;
+        this.nodeDao = nodeDao;
+        this.alarmDao = alarmDao;
+        this.eventSubscriptionService = eventSubscriptionService;
+
+    }
+
+    public void start(){
+        eventSubscriptionService.addEventListener(this);
+    }
+
 	@Override
 	public String getName() {
 
-		return "AcomEventIngestor";
+		return "acomEventIngestor";
 	}
 
 	@Override
 	public int getNumThreads() {		
-		return threads;
+		return 1;
 	}
 
 	@Override
 	public void onEvent(InMemoryEvent e) {
-		
+
+
+
 		if(!e.getUei().startsWith(UEI_ACOM_MATCH)) {
 			return;
 		}
-	
-		
-		
-		eventForwarder.sendAsync(  translate(e)  );
+
+        LOG.info("arrive new event: {}", e);
+
+        ImmutableInMemoryEvent translate = translate(e);
+
+        LOG.info("send event translated: {} ", translate);
+        eventForwarder.sendAsync( translate );
 			
 	}
-	
+
+
 	private ImmutableInMemoryEvent translate(InMemoryEvent e) {
-		
+
+        LOG.info("translate event : {}", e);
 		String nodeLabel = e.getParametersByName(NODE_LABEL_ACOM_PARAMETER_MATCH).get(0).getValue();
 		Node nodeByLabel = nodeDao.getNodeByLabel(nodeLabel);
 		
 		String uei = e.getUei().replace(UEI_ACOM_MATCH, UEI_ACOM_MATCH + "/translator");
 		
 		return ImmutableInMemoryEvent.newBuilderFrom(e)
-				.setNodeId(nodeByLabel.getId())
+				.setNodeId(nodeByLabel == null ? 1 : nodeByLabel.getId())
 				.setUei(uei)
 				.build();
 	} 
@@ -85,12 +104,7 @@ public class AComEventIngestore implements EventListener, Runnable {
 	
 	public synchronized void endSync() {
 		syncAct = false;
-	} 
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
 	}
+
 
 }
